6996d35f70e5a7d8d2f9f83914957759b80f5412
DoniyorLatipov <doniyor.main.contact@gmail.com>
2026-05-01 22:42:44 +0300

    feat(utils): add mapper for converting form data to create task DTO

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,5 +1,7 @@
 import { clsx, type ClassValue } from 'clsx';
 import { twMerge } from 'tailwind-merge';
+import { CreateTaskInputType } from './types';
+import { BasePrioritiesType } from '@/config/priorities';
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
@@ -7,4 +9,14 @@ export function cn(...inputs: ClassValue[]) {
 
 export function capitalize(text: string): string {
   return text.charAt(0).toUpperCase() + text.slice(1).toLowerCase();
+}
+
+export function mapFormDataToCreateTaskInputType(formData: FormData): CreateTaskInputType {
+  return {
+    title: String(formData.get('title') || ''),
+    description: String(formData.get('description') || ''),
+    assignee: String(formData.get('assignee') || ''),
+    priority: formData.get('priority') as BasePrioritiesType,
+    dueDate: String(formData.get('dueDate') || ''),
+  };
 }
