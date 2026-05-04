709954185395e4ebd2b1c4f6e382cfcab2192fb1
DoniyorLatipov <doniyor.main.contact@gmail.com>
2026-05-04 11:51:01 +0300

    feat(utils): add getPriorityColorClass with priority-to-color mapping

--- lib/getColorClass.ts
+++ lib/getColorClass.ts
@@ -1,8 +1,19 @@
 import { BaseColorType } from '@/config/color';
 import { colorClassMap } from './twColorMapper';
+import { BasePrioritiesType } from '@/config/priorities';
 
 type Variant = 'bg' | 'text';
 
 export function getColorClass(color: BaseColorType, variant: Variant) {
   return colorClassMap[color][variant];
+}
+
+const priorityMapper: Record<BasePrioritiesType, BaseColorType> = {
+  high: 'red',
+  medium: 'yellow',
+  low: 'green',
+};
+
+export function getPriorityColorClass(priority: BasePrioritiesType) {
+  return getColorClass(priorityMapper[priority], 'bg');
 }
